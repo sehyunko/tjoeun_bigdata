25be40f48f0d442b2993713b22847bfa7697709c
sehyunko <85028719+sehyunko@users.noreply.github.com>
2021-05-30 19:21:56 +0900

    Add files via upload

---
+++ Java/ch06_inheritance/Ex01_inheritance_branch.java
@@ -0,0 +1,28 @@
+package ch06_inheritance;
+
+public class Ex01_inheritance_branch extends Ex01_inheritance_HQ {
+	
+	
+	//setters 
+	public void setKimchi(int kimchi) {
+		this.kimchi = kimchi;
+	}
+
+	public void setSunde(int sunde) {
+		this.sunde = sunde;
+	}
+
+	public void setBude(int bude) {
+		this.bude = bude;
+	}
+
+	public void setDbokki(int dbokki) {
+		this.dbokki = dbokki;
+	}
+	
+	
+
+	
+	
+	
+}
