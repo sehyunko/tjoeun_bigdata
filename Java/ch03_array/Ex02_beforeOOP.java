142a85231f478f2217df240ddee4ae700bf93730
sehyunko <85028719+sehyunko@users.noreply.github.com>
2021-05-30 13:32:29 +0900

    Add files via upload

---
+++ Java/ch03_array/Ex02_beforeOOP.java
@@ -0,0 +1,26 @@
+package ch03_array;
+
+public class Ex02_beforeOOP {
+	private int input;
+
+	public Ex02_beforeOOP(int input) {
+		this.input = input;
+	}
+	
+	public void printGugudan() {
+		for(int i=1; i<10;i++) {
+			System.out.println(input+"x"+i+"="+input*i);
+		}
+	}
+
+	public int getInput() {
+		return input;
+	}
+
+	public void setInput(int input) {
+		this.input = input;
+	} 
+	
+	
+	
+}
---
+++ Java/ch03_array/GugudanTestMain.java
@@ -0,0 +1,16 @@
+package ch03_array;
+
+import java.util.Scanner;
+
+public class GugudanTestMain {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("원하는 단수를 입력하세요 (2~9)");
+		int input = scanner.nextInt(); 
+		Ex02_beforeOOP gugudan = new Ex02_beforeOOP(input);
+		gugudan.printGugudan();
+		scanner.close(); 
+	}
+
+}
