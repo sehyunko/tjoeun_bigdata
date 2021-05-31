f628ee132c194850803490174fbdd779ca006149
sehyunko <85028719+sehyunko@users.noreply.github.com>
2021-05-31 17:27:51 +0900

    Add files via upload

---
+++ Java/ch09_pattern/AClass.java
@@ -0,0 +1,22 @@
+package singleton.step1;
+
+public class AClass {
+	private int intVar;
+	private static AClass INSTANCE; //=new AClass(); 
+
+	public static AClass getInstance() {
+		if(INSTANCE == null) {
+			INSTANCE = new AClass();
+		}
+		return INSTANCE; 
+	}
+	public void setIntVar(int intVar) {
+		this.intVar = intVar; 
+	}
+	public int getIntvar() {
+		return intVar;
+	}
+	
+	
+
+}
