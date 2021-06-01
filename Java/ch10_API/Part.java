125ee4e2ee33b488b14b51a8bfc1ce86af8311bc
sehyunko <85028719+sehyunko@users.noreply.github.com>
2021-06-01 14:33:06 +0900

    Delete Java/ch10_API directory

--- Java/ch10_API/Part.java
+++
@@ -1,9 +0,0 @@
-package com.lect.API;
-
-public class Part {
-	public static final String COMPUTER="전산부";
-	public static final String PLANNING="기획부";
-	public static final String DESIGN="설계부";
-	public static final String ACCOUNTING="회계부";
-	public static final String HUMANRESOURCES="인사부";
-}
