66a28208847f661f8b81f604186a819d857efa6d
sehyunko <85028719+sehyunko@users.noreply.github.com>
2021-06-20 19:35:41 +0900

    Add files via upload

---
+++ 01_Java/src/ch10_API/Part.java
@@ -0,0 +1,12 @@
+package gitupload;
+
+/*사원들의 사원번호, 이름, 부서를 출력받고
+ *객체를 생성한 시간을 입사일로 해서 각 사원의 
+ *사번,이름,부서,입사일을 출력하는 프로그램을 짜보자*/
+public class Part {
+	public static final String COMPUTER="전산부";
+	public static final String PLANNING="기획부";
+	public static final String DESIGN="설계부";
+	public static final String ACCOUNTING="회계부";
+	public static final String HUMANRESOURCES="인사부";
+}
