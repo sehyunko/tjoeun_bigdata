2c80f00f5a3cf30efe4985bb8d05dd408d3bf492
sehyunko <85028719+sehyunko@users.noreply.github.com>
2021-06-20 19:11:49 +0900

    Add files via upload

---
+++ 01_Java/src/ch02_control/Ex01_switch.java
@@ -0,0 +1,33 @@
+package gitupload;
+
+/*실행할  마다 난수 점수(0~100점)를 발생시켜 
+ * 학점을 출력하는 프로그램을 만드는 예제*/
+
+public class Ex01_switch {
+
+	public static void main(String[] args) {
+		int score; 
+		score = (int)(Math.random()*101);
+		System.out.println("원 점수는: "+score);
+		switch(score/100) {
+		case 10:
+		case 9:
+			System.out.println("학점은 A");
+		break; 
+		case 8:
+			System.out.println("학점은 B");
+		break; 
+		case 7:
+			System.out.println("학점은 C");
+		break; 
+		case 6: 
+			System.out.println("학점은 D");
+		default:
+			System.out.println("학점은 F");
+		break;
+		
+		}
+
+	}
+
+}
